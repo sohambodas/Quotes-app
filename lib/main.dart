b1d116d125af7f82ffd462f16dbc4d1bb60e0ed2
Soham Bodas <38501367+sohambodas@users.noreply.github.com>
2020-11-26 21:11:48 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Soham',text: 'an apple a day keeps doctor away'),
+    Quote(author: 'Tushar',text: 'a pineapple a day keeps police away'),
+    Quote(author: 'Bodas',text: 'a jackfruit a day keeps everyone away'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
+      ),
+    );
+  }
+}
+
